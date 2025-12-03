386672161ef4912b6ad246e57f1104e685cdaa56
sachin123112 <songforswag@gmail.com>
2025-12-03 12:48:33 +0530

    Update cl.py

--- cl.py
+++ cl.py
@@ -1,40 +1,4 @@
-print("----------cal----------")
-
-#a=int(input("ENTER A-VALUE :"))
-#b=int(input("ENTER B-VALUE :"))
-a=10
-b=20
-
-print("select option to operation \n 1.+ \n 2.- \n 3.* \n 4./ \n 5.> \n 6.< \n 7.%\n 8.0")
-#option=int(input("select option "))
-while option != 0:
-   
-
-    if option ==1:
-        print(f" value is :{a + b}")
-    elif option ==2:
-        print(f"value is :{a - b}")
-    elif option ==3:
-        print(f"value is :{a * b}")
-    elif option ==4:
-        if b != 0:
-            print(f"Result: {a / b}")
-        else:
-            print("Error: Division by zero is not allowed.")
-    
-    elif option ==5:
-        print(f"value is :{a > b}")
-    elif option ==6:
-        print(f"value is :{a < b}")
-    elif option ==7:
-        print(f"value is :{a % b}")
-    elif option==8:
-        print("Exiting... Goodbye!")
-        exit
-        
-    else:
-        print("select corret option")
-        continue
-    
-    #option=int(input("select option "))
-
+a = 10
+b = 20
+c = a + b
+print("Sum:", c)
